fe07141322e0b0b8ae0d6a5ad73a175ac1a3573c
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-29 00:37:55 +0000

    render

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_28_223558) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "uid"
-    t.string "pass"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
