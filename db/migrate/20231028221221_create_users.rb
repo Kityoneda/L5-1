f49a2af80083fa0a519234bb320aae9aed857881
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-29 02:37:45 +0000

    render

--- db/migrate/20231028221221_create_users.rb
+++ db/migrate/20231028221221_create_users.rb
@@ -1,6 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists?(:users)
       create_table :users do |t|
         t.string :uid
         t.string :pass
@@ -8,6 +7,5 @@ class CreateUsers < ActiveRecord::Migration[7.0]
         t.timestamps
       end
       add_index :users, :uid, unique: true
-    end
   end
 end
