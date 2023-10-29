f69881c91eca0d35f571bf4e0c9492e9402fe3f9
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-29 02:25:50 +0000

    render

--- db/migrate/20231028223558_create_likes.rb
+++ db/migrate/20231028223558_create_likes.rb
@@ -1,12 +1,10 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists?(:likes)
     create_table :likes do |t|
       t.integer :tweet_id
       t.integer :user_id
 
       t.timestamps
-     end
     end
   end
 end
