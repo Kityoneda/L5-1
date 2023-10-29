07ebf72128466b266d722ae6f7cc9e6ba843bd21
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-10-29 02:21:17 +0000

    render

--- db/migrate/20231028222621_create_tweets.rb
+++ db/migrate/20231028222621_create_tweets.rb
@@ -1,6 +1,6 @@
 class CreateTweets < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists?(:users)
+    unless table_exists?(:tweets)
     create_table :tweets do |t|
       t.string :message
       t.integer :user_id
--- db/migrate/20231028223558_create_likes.rb
+++ db/migrate/20231028223558_create_likes.rb
@@ -1,6 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    unless table_exists?(:users)
+    unless table_exists?(:likes)
     create_table :likes do |t|
       t.integer :tweet_id
       t.integer :user_id
