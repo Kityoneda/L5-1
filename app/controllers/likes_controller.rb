c57ddb19bbe80d8aef6e5270fbf54f2f578bd2eb
Kazuki Yoneda <2111140039c@ed.fuk.kindai.ac.jp>
2023-11-01 07:24:03 +0000

    additional word

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,17 +4,13 @@ class LikesController < ApplicationController
     unless tweet.liked?(current_user)
       tweet.like(current_user)
     end
-    user = User.find_by(uid: current_user)
-    user.like_tweets << tweet
     redirect_to tweets_path
   end
   def destroy
     tweet = Tweet.find(params[:id])
     if tweet.liked?(current_user)
-      tweet.like(current_user)
+      tweet.unlike(current_user)
     end
-    user = User.find_by(uid: current_user)
-    tweet.likes.find_by(user_id: user.id).destroy
     redirect_to tweets_path
   end
 end
